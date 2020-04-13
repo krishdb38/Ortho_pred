0160110fc5ba46bbd3c51f539c4069dcae940600
krishdb38 <45994360+krishdb38@users.noreply.github.com>
2020-04-13 17:15:57 +0900

    Update owPReMark_GUI.py

--- GUI_TK/owPReMark_GUI.py
+++ GUI_TK/owPReMark_GUI.py
@@ -1,8 +1,9 @@
 
 import random
-import time
-import tkinter as tk
 
+import tkinter as tk
+import time
+from time import strftime
 
 bigLabels = ["Ortho_Detection"]
 medLabels = ["Mode"]
@@ -22,17 +23,25 @@ mode_input = tk.StringVar()
 Tops = tk.Frame(window, width=1600,height =50,bg="powder blue")
 Tops.pack(side = "top")
 
-# frame 1
+# frame 1 for main Software
 f1 = tk.Frame(window,width = 800,height = 700,bg = "light blue").pack(side = "left")
 
-# Frame 2
+# Frame 2 for extra Features
 f2 = tk.Frame(window,width = 300,height = 700,bg= "light green").pack(side= "right")
+
+prog_label = tk.Label(Tops,font = ("arial",45,"bold"),text = "owPReMark V1.0",fg = "blue")
+prog_label.grid(row=0, column=0)
+
 # !! Current Time
-local_time = time.asctime(time.localtime(time.time()))
-prog_name = tk.Label(Tops,font = ("arial",40,"bold"),text = "owPReMark V1.0",fg = "blue", bd = 10)
+def time_():
+    pass
+    #string_ = strftime("%H:%M:%S %p")
+    #time_label.config(text = string_)
+    #time_label.after(1000, time)
 
-prog_name.grid(row=0, column=0)
+local_time=time.asctime(time.localtime(time.time()))
+time_label = tk.Label(Tops,font = ("arial",18,"bold"),text ="Started_Time-->"+local_time,fg = "white",bg = "purple", bd =10)
+time_label.grid(row=1,column=0)
 
-
-
+# time() for live time
 window.mainloop()
